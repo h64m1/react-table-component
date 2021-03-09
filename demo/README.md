8b6a65d259ec15013c217395934c942ea9f5c7f8
mass8 <29457120+h64m1@users.noreply.github.com>
2021-03-09 22:04:29 +0900

    demo page
    - use vite (template=react-ts)
    - development: npm run dev

---
+++ demo/README.md
@@ -0,0 +1,13 @@
+
+# @h64m1/react-table-component demo page
+
+## environment setup by vite
+$ npm init @vitejs/app
+select react-ts template
+
+
+## development
+```
+$ npm i
+$ npm run dev
+```
---
+++ demo/src/App.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Column, Table } from '@h64m1/react-table-component'
+
+const App: React.FC = () => {
+	return (
+		<>
+			<DemoTableNoBorders />
+			<DemoTableBorders />
+		</>
+	)
+}
+
+const DemoTableNoBorders: React.FC = () => {
+	type Data = {
+		string: string
+		integer: number
+		float: number
+		function: string
+		dom: React.ReactElement
+	}
+	const test = (arg: string): string => {
+		return arg
+	}
+
+	const data: Data[] = [
+		{
+			string: 'hello',
+			integer: 0,
+			float: 1.528,
+			function: test('test1'),
+			dom: <input />,
+		},
+		{
+			string: 'world',
+			integer: 1,
+			float: 2.696,
+			function: test('test2'),
+			dom: (
+				<select>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+				</select>
+			),
+		},
+		{
+			string: 'test',
+			integer: 2,
+			float: 9.111,
+			function: test('test3'),
+			dom: (
+				<div>
+					<span>span</span>
+				</div>
+			),
+		},
+	]
+	return (
+		<Table data={data}>
+			<Column name="integer" />
+			<Column name="float" />
+			<Column name="string" />
+			<Column name="function" />
+			<Column name="dom" />
+		</Table>
+	)
+}
+
+type Data = {
+	column1: string
+	column2: string
+	column3: string
+	column4: string
+}
+
+const data: Data[] = [
+	{
+		column1: '1',
+		column2: '2',
+		column3: '3',
+		column4: '4',
+	},
+	{
+		column1: '5',
+		column2: '6',
+		column3: '7',
+		column4: '8',
+	},
+	{
+		column1: '9',
+		column2: '10',
+		column3: '11',
+		column4: '12',
+	},
+	{
+		column1: '13',
+		column2: '14',
+		column3: '15',
+		column4: '16',
+	},
+]
+
+const DemoTableBorders: React.FC = () => {
+	return (
+		<Table data={data} className="border">
+			{[1, 2, 3, 4].map((e) => (
+				<Column key={e} name={`column${e}`} className="border" />
+			))}
+		</Table>
+	)
+}
+
+export default App
