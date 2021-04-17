1ec4baa79d45f72a491d489552895d0443c70f7f
mass8 <29457120+h64m1@users.noreply.github.com>
2021-04-17 14:59:51 +0900

    remove console.debug in render

--- src/component/table/Table.tsx
+++ src/component/table/Table.tsx
@@ -23,7 +23,6 @@ type TableProps<Data> = {
  */
 const Table = <T extends object>(props: TableProps<T>) => {
 	const data = props.data
-	console.debug('Table | props', props)
 
 	if (data === undefined || (data !== undefined && data.length === 0)) {
 		console.debug('Table | data is empty. Do not draw table')
